e97ac3e97bb812b8d5dec9ca2ca750ee89848fe4
NewBase64 <Acevedomatthew474@gmail.com>
2022-06-17 18:24:57 -0400

    ugh

---
+++ Calculator/Processor.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include "Calculator.h"
+#include "App.h"
+	class Processor
+{
+private:
+	static Processor* processor;
+
+	int dAT;
+	int num1;
+	int num2;
+
+	Processor() {}
+public:
+
+	static Processor* GetProcess() {
+		if (processor == nullptr)
+			processor = new Processor;
+	}
+	void SetBaseNumber(int number) {
+
+
+
+	}
+
+	Processor(Processor& other) = delete;
+	void operator=(const Processor& other) = delete;
+
+
+
+
+
+
+
+
+};
+Processor* Processor::processor = nullptr;
